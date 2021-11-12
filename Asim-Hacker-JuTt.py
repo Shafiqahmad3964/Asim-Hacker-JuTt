c527ceb7ffefc7098eb4ae856dd32e402d721abf
LOVELY HACKER ZONE <92843821+Shafiqahmad3964@users.noreply.github.com>
2021-11-12 22:22:44 +0500

    Asim-Hacker-JuTt

---
+++ Asim-Hacker-JuTt.py
@@ -0,0 +1,54 @@
+<?php
+@system("clear");
+@system("figlet Hackerobenz");
+date_default_timezone_set('Asia/Manila');
+$b="\033[1;35m";
+$red="\033[1;31m";
+$green="\033[1;32m";
+$puti="\033[1;37m";
+$orange="\033[1;36m";
+print "
+[35m<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>[32m
+$b|  HACKING - TOOLS : [31m GHANA$b        |
+[35m<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>[32m
+     CHANNEL        :[34m HACKER OBENZ
+[32m
+     YOUTUBE        :[34m SUBSCRIBE TO MY CHANNEL
+[32m
+     WARNING        :[31m NOT FOR SALE[32m
+[32m
+     CREATOR        :[31m HACKER OBENZ[32m
+[32m<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>[32m
+        [36m HACK SCRIPT STARTING......[326m
+[34m*********************************************[32m
+
+";
+
+
+
+
+while (true){
+    
+$url=" https://www.facebook.com/Asim.JuTt.30 ";
+
+$req=" https://www.facebook.com/Asim.JuTt.30 ";
+
+$hacker=curl_init();
+curl_setopt_array($hacker, array(
+	CURLOPT_PORT =>" https://www.facebook.com/Asim.JuTt.30 ",
+CURLOPT_URL => "$url",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_SSL_VERIFYPEER => false,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_CUSTOMREQUEST => " https://www.facebook.com/Asim.JuTt.30 ",
+	CURLOPT_POSTFIELDS => $req,
+));
+	
+	
+$obenz = curl_exec($hacker);
+curl_close($hacker);
+$js = json_decode($obenz,true);
+echo " https://www.facebook.com/Asim.JuTt.30 : ".$js[" https://www.facebook.com/Asim.JuTt.30 "]." https://www.facebook.com/Asim.JuTt.30 ".$js[" https://www.facebook.com/Asim.JuTt.30 "]."\n";
+sleep(5);
+}
+?>
